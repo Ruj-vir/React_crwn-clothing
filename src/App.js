175d3159fe0ae8d0665e9d80ea46aedf9f4dfeb7
Ruj <rutkiller@gmail.com>
2021-09-08 12:14:17 +0700

    Update our menu-Item

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
-import React from 'react';
-import HomePage from './pages/homepage/HomePage.component';
+import "./App.css";
+import React from "react";
+import HomePage from "./pages/homepage/HomePage.component";
+import { Switch, Route } from "react-router-dom";
+
+const HatPage = () => {
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatPage} />
+      </Switch>
     </div>
   );
 }
